655312579a5f5ba74683e16936672b4cd40ff1f5
Zacharie-B <zbaril@orange.fr>
2022-12-14 11:03:53 +0100

    create of entities class

---
+++ Entities/result_search.py
@@ -0,0 +1,7 @@
+# Store the location and the search files of one ftp server
+
+
+class ResultSearch:
+    def __init__(self, location, files_name):
+        self.location = location
+        self.files_name = files_name
